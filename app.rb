5e907dae3d5815eac025603785d9b9f2de96a4e1
Pat Allan <pat@freelancing-gods.com>
2013-05-07 11:17:03 -0700

    More tweaks, but not yet fixed afaik.

--- app.rb
+++ app.rb
@@ -2,7 +2,13 @@ require 'bundler'
 
 Bundler.require :default, :development
 
+Sinatra::Application.environment = ENV['RACK_ENV'] || :development
+
 config = YAML.load(File.read('config/database.yml'))
 ActiveRecord::Base.establish_connection(
   config[Sinatra::Application.environment.to_s]
 )
+
+class MyApp < Sinatra::Base
+  #
+end
